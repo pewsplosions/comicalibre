4c6e5478776df83ba7c05cf982852f2c54d8a8db
Pewsplosions <pewsplosions@gmail.com>
2019-10-30 12:39:55 -0400

    Removed unused imports

--- src/work/main.py
+++ src/work/main.py
@@ -1,8 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import sys
-import traceback
 from threading import Thread
 
 from calibre_plugins.comicalibre.ui.config import prefs
